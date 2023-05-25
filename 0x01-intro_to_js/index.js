f43761db04326a6c994fff2b2832a0d0756270c4
Alexander Atuobi <alexanderatuobi2@gmail.com>
2023-05-25 21:20:21 +0000

    task-0x03

---
+++ 0x01-intro_to_js/index.js
@@ -0,0 +1,16 @@
+let myName = 'Alexander';
+typeof myName;
+myName = 'Atuobi';
+
+const car = {
+    make: 'Toyota',
+    model: 'Camry',
+    year: 2022 ,
+    bodytype: 'Sedan',
+    engine: '3.5L V6',
+    fuelType: 'hybrid',
+    Transmission: 'automatic',
+    SeatingCapacity: 4,
+    features: ['air conditioning', 'backup camera', 'sunroof', 'navigation sSystem'],
+    safetyFeature: ['airbags', 'ABS', 'tracting Control']
+};
---
+++ 0x01-intro_to_js/numbers.js
@@ -0,0 +1,7 @@
+const favoriteNumber = 8;
+const halfOfFavNum = favoriteNumber / 2 ;
+
+let isaacsFavorite = 13;
+isaacsFavorite - favoriteNumber;
+isaacsFavorite *= 26;
+isaacsFavorite %= 26;
